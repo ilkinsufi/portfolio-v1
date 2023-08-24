45a33c3dc13b8bc4a60d76b07e149482c8bc8bf4
ilkisufi <ilkinsufi@gmail.com>
2023-08-24 18:24:55 +0400

    updated again

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ function downloadcv() {
   console.log("salam");
   window.location.href =
     // "https://drive.google.com/uc?export=download&id=1tbm4rY_IFjUHrejFaucTLpzbcCSZOEDU";
-    "https://drive.google.com/file/d/12DtNx_72EkJg1-I61f1RxfC8VMeAypkJ/view?usp=sharing";
+    // "https://drive.google.com/file/d/12DtNx_72EkJg1-I61f1RxfC8VMeAypkJ/view?usp=sharing";
+    "https://drive.google.com/file/d/12DtNx_72EkJg1-I61f1RxfC8VMeAypkJ/view";
 }
 
 // sidebar variables
